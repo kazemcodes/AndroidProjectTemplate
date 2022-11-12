6513409cf7888638834de2e4192f4dceb601182f
KazemM <76521159+kazemcodes@users.noreply.github.com>
2022-11-12 08:39:44 +0330

    update repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
+        maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+        maven(url ="https://repo1.maven.org/maven2/")
     }
     versionCatalogs{}
 }
